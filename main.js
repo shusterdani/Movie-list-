14a254aa1a7b506aaecb052b80f1008089cb702b
Dani Shuster <shusterdani@gmail.com>
2025-04-22 22:29:11 +0300

    edit button added not functional yet

--- main.js
+++ main.js
@@ -22,7 +22,11 @@ function pushMovie() {
     const director = directorName.value;
     const desc = description.value;
 
-    let movie = {title, director, desc};
+    let movie = {
+        movieName: title,
+        directorName: director,
+        description: desc
+    };
 
     moviesList.push(movie);
 }
@@ -31,17 +35,43 @@ function displayMovie() {
     let content = "";
             for(let i = 0; i < moviesList.length; i++) {
             const tr = `
-                <tr>
+                <tr class="parentRow">
                     <td>${moviesList[i].title}</td>
                     <td>${moviesList[i].director}</td>
                     <td>${moviesList[i].desc}</td>
                     <td><button onclick="deleteMovie(${i})">X</button></td>
+                    <td><button onclick="editMovie(${i})" class="edit-btn">✍🏻</button></td>
                 </tr>            
             `;
             content += tr;
         }
     containerTbody.innerHTML = content;
 }
+
+function editMovie(index) {
+    let editArea = "";
+    for(let i = 0; i < moviesList.length; i++) {
+        if(i === index){    
+            const tr = `
+                <tr class="parentRow">
+                    <td> <input type="text" placeholder="${moviesList[i].title}" id="movieName"</td>
+                    <td> <input type="text" placeholder="${moviesList[i].director}"</td>
+                    <td> <input type="text" placeholder="${moviesList[i].desc}"</td>
+                    <td><button onclick="saveBack(${i})" class="save-back">📥</button></td>
+                    <td><button onclick="saveBack(${i})" class="save-back">🔙</button></td>
+                </tr>  
+            `
+            editArea += tr;
+        }
+    }
+    containerTbody.innerHTML = editArea;
+}
+
+// function saveBack(index) {
+   
+// }
+
+
 
 function saveMoviesList() {
     const json = JSON.stringify(moviesList);
